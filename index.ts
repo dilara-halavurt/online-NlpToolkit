495e0388273667401899f556d155011028f60924
Dilara Halavurt <dilarahalavurt@192.168.1.46>
2024-02-19 14:42:06 +0300

    update on port

--- index.ts
+++ index.ts
@@ -1,22 +1,23 @@
 import express from 'express';
 import {SimpleSpellChecker} from "nlptoolkit-spellchecker/dist/SimpleSpellChecker";
 import { FsmMorphologicalAnalyzer } from 'nlptoolkit-morphologicalanalysis/dist/MorphologicalAnalysis/FsmMorphologicalAnalyzer';
-import { Sentence } from 'nlptoolkit-corpus/dist/Sentence'
+import { Sentence } from 'nlptoolkit-corpus/dist/Sentence';
 const cors = require('cors');
 const app = express();
 
-const PORT = 3001;
+// Use Heroku's process.env.PORT or 3001 if there's nothing there.
+const PORT = process.env.PORT || 3001;
 const fsm = new FsmMorphologicalAnalyzer();
-const spellChecker = new SimpleSpellChecker(fsm);   
-
+const spellChecker = new SimpleSpellChecker(fsm);
 
 app.use(cors()); // Enable CORS
 app.use(express.json()); // Parse JSON bodies
 
-
+// Modified to listen on process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
+
 // Example endpoint for text correction
 app.post('/correct-text', (req, res) => {
   const { text } = req.body;
